c3e3735f6574ba8181cbc06d664080296ac1241b
HiraTariqAkhtar <hira.akhtar@student.ehb.be>
2023-05-15 14:33:15 +0200

    Get random recipes

--- pages/Recipes.js
+++ pages/Recipes.js
@@ -18,13 +18,157 @@ import { Ionicons, FontAwesome } from "@expo/vector-icons";
 export default class Recipes extends React.Component {
   constructor(props) {
     super(props);
+    this.getRecipes()
+
+
+    this.state = {
+      randomRecipes: [
+        {
+          id:0,
+          servings: 0,
+          recipeName: "",
+          ingredients: [{
+            id: 0,
+            nameAndQuantity: "",
+            img: "",
+          }],
+          instructions: [],
+          culture: [],
+          time: 0,
+          foodImg: "",
+          dishTypes: [],
+          period: []
+        },
+      ],
+    };
   }
 
 
+  async getRecipes() {
+    axios.get("https://api.spoonacular.com/recipes/random?number=5&apiKey=284bcd786d6c49be8d0ca1c4ba98db07")
+    .then((res) => {
+      res.data.recipes.forEach((rec) => {
+        this.state.randomRecipes.push({
+          id: rec.id,
+          servings: rec.servings,
+          recipeName: rec.title,
+          ingredients: rec.extendedIngredients,
+          instructions: rec.analyzedInstructions[0].steps,
+          culture: rec.cuisines,
+          time: rec.readyInMinutes,
+          foodImg: rec.image,
+          dishTypes: rec.dishTypes,
+          period: rec.occasions
+        })
+      })
+      
+      //console.log(this.state.randomRecipes[1])
+      this.setState({state: this.state})
+    })
+  }
+
+  goToRecipeDetails(rec) {
+    this.props.navigation.navigate("RecipeDetail", {
+      recipeName: rec.recipeName,
+      foodImg: rec.foodImg,
+      servings: rec.servings,
+      timeNeeded: rec.time,
+      dishTypes: rec.dishTypes,
+      period: rec.period,
+      culture: rec.culture,
+      ingredients: rec.ingredients,
+      instructions: rec.instructions
+    })
+  }
+
   render() {
+
+    let recipes = this.state.randomRecipes
+    .slice(1)
+    .map((rec) => (
+      <TouchableOpacity
+      key={rec.id}
+      style={styles.recipe}
+      onPress={() => this.goToRecipeDetails(rec)}>
+        <View>
+        <Image
+          source={{uri: rec.foodImg}}
+          style={styles.foodImg}
+          />
+
+        <Text style={styles.recipeName}>
+            {rec.recipeName}
+        </Text>
+
+         
+          <View style={styles.info}>
+          <View style={styles.iconText}>
+            <Ionicons
+              name={"people"}
+              size={hp("5%")}
+              color="#34359A"
+            />
+              <Text style={styles.text}>{rec.servings}</Text>
+          </View>
+
+            <View style={styles.iconText}>
+              <Ionicons
+              name={"stopwatch"}
+              size={hp("5%")}
+              color="#34359A"
+            />
+              <Text style={styles.text}>{rec.time} minutes</Text>
+            </View>
+          </View>
+
+            {rec.dishTypes.length > 0 && (
+            <View style={styles.iconText}>
+            <FontAwesome
+              name={"cutlery"}
+              size={hp("4%")}
+              color="#34359A"
+              marginRight={wp("1%")}
+            />
+              {rec.dishTypes.map((type) => (
+                <Text style={styles.text}>{type}</Text>
+              ))}
+            </View>)}
+
+           {rec.period.length > 0 && (
+           <View style={styles.iconText}>
+            <Ionicons
+              name={"calendar"}
+              size={hp("4%")}
+              color="#34359A"
+              marginRight={wp("1%")}
+            />
+              {rec.period.map((period) => (
+                <Text style={styles.text}>{period}</Text>
+              ))}
+            </View>)}
+
+            {rec.culture.length > 0 && (
+            <View style={styles.iconText}>
+            <Ionicons
+              name={"flag"}
+              size={hp("4%")}
+              color="#34359A"
+              marginRight={wp("1%")}
+            />
+              {rec.culture.map((type) => (
+                <Text style={styles.text}>{type}</Text>
+              ))}
+            </View>)}
+        </View>
+      </TouchableOpacity>
+    ))
 
     return (
       <View style={styles.container}>
+        <Text style={styles.pageTitle}>Recipes</Text>
+        <ScrollView>
+          {recipes}
+        </ScrollView>
       </View>
     );
   }
@@ -35,6 +179,52 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
+    marginBottom: hp("10%"),
+    marginTop: hp("5%")
     
   },
+  pageTitle: {
+    fontSize: hp("4%"),
+    color: "#34359A",
+    fontFamily: "Nunito_700Bold",
+    marginBottom: hp("1%"),
+    textAlign: "center"
+  },
+  recipe: {
+    backgroundColor: "white",
+    padding: hp("3%"),
+    width: wp("85%"),
+    borderRadius: 10,
+    marginTop: hp("3%"),
+  },
+  foodImg: {
+    width: wp("65%"),
+    height: hp("25%"),
+    marginBottom: hp("2%"),
+    marginHorizontal: wp("5%")
+  },
+  recipeName: {
+    fontSize: hp("3.5%"),
+    color: "#34359A",
+    fontFamily: "Nunito_700Bold",
+    marginBottom: hp("1%"),
+    textAlign: "center"
+  },
+  info: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-evenly"
+  },
+  iconText: {
+    display: "flex",
+    flexDirection: "row",
+    flexWrap: "wrap",
+    alignItems: "center",
+    marginBottom: hp("1%"),
+  },
+  text: {
+    fontFamily:"Nunito_400Regular",
+    fontSize: hp("2%"),
+    marginLeft: wp("2%")
+  }
 });
