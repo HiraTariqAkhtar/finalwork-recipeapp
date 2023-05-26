fa05c42df6392e6472fea1eee5276e4b526b38b4
HiraTariqAkhtar <hira.akhtar@student.ehb.be>
2023-05-26 12:05:17 +0200

    Add font: Nunito Light Italic

--- App.js
+++ App.js
@@ -14,6 +14,7 @@ import {
 import {
   useFonts,
   Nunito_300Light,
+  Nunito_300Light_Italic,
   Nunito_400Regular,
   Nunito_500Medium,
   Nunito_600SemiBold,
@@ -137,6 +138,7 @@ const Tabs = () => {
 export default function App() {
   let [fontsLoaded] = useFonts({
     Nunito_300Light,
+    Nunito_300Light_Italic,
     Nunito_400Regular,
     Nunito_500Medium,
     Nunito_600SemiBold,
