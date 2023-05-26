793a9e14ced33cfc33fbff6f226e74cb1d62623f
HiraTariqAkhtar <hira.akhtar@student.ehb.be>
2023-05-26 12:05:46 +0200

    Create basic login/register page

--- pages/LogIn.js
+++ pages/LogIn.js
@@ -3,7 +3,7 @@ import {
   StyleSheet,
   View,
   TouchableOpacity,
-  ScrollView,
+  TextInput,
   Text,
   Image
 } from "react-native";
@@ -14,10 +14,20 @@ import {
 import axios from "axios";
 import { Ionicons, FontAwesome } from "@expo/vector-icons";
 
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default class Login extends React.Component {
   constructor(props) {
     super(props);
+  }
+
+  register() {
+    this.props.navigation.navigate("Register")
+  }
+
+  logIn() {
+    AsyncStorage.setItem("userLoggedIn", "true")
+    this.props.navigation.navigate("Profile")
   }
 
 
@@ -25,6 +35,26 @@ export default class Login extends React.Component {
 
     return (
       <View style={styles.container}>
+          <Text style={styles.title}>Sign In</Text>
+
+          <View style={{display:"flex", flexDirection:"row", justifyContent:"center"}}>
+              <Text style={styles.question}>Not a user yet?</Text>
+              <Text style={styles.nav} onPress={() => this.register()}>Sign Up</Text>
+          </View>
+
+          <Text style={styles.text}>Email address:</Text>
+          <TextInput
+          style={styles.placeholder}
+          placeholder="Enter email address"/>
+
+          <Text style={styles.text}>Password:</Text>
+          <TextInput
+          style={styles.placeholder}
+          placeholder="Enter password"/>
+
+          <TouchableOpacity style={styles.button} onPress={() => this.logIn()}>
+            <Text style={styles.btnText}>Sign in</Text>
+          </TouchableOpacity>
       </View>
     );
   }
@@ -33,8 +63,51 @@ export default class Login extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    
+    marginTop: hp("8%")    
   },
+  title: {
+    textAlign: 'center',
+    fontFamily: "Nunito_700Bold",
+    fontSize: hp("3.5%"),
+    marginBottom: hp("5%")
+  },
+  question: {
+      fontFamily: "Nunito_600SemiBold",
+      fontSize: hp("2%"),
+      marginBottom: hp("5%"),
+      marginRight: wp("5%")
+  },
+  nav: {
+      fontFamily: "Nunito_300Light_Italic",
+      fontSize: hp("2%"),
+      marginBottom: hp("5%"),
+      marginRight: wp("5%"),
+      textDecorationLine: "underline"
+  },
+  text: {
+      fontFamily: "Nunito_700Bold",
+      marginBottom: hp("1%"),
+      marginLeft: wp("3%")
+  },
+  placeholder: {
+    height: hp("5%"),
+    borderWidth: 1,
+    padding: wp("2%"),
+    marginHorizontal: wp("3%"),
+    marginBottom: hp("3%")
+  },
+  button: {
+    width: wp("80%"),
+    padding: hp("1%"),
+    backgroundColor: "#34359A",
+    borderRadius: wp("50%"),
+    marginHorizontal: wp("10%"),
+    marginTop: hp("5%"),
+  },
+  btnText:{
+    fontFamily:"Nunito_400Regular",
+    fontSize: hp("2.5%"),
+    color: "#ffffff",
+    textAlign: "center"
+  }
 });
--- pages/Register.js
+++ pages/Register.js
@@ -3,21 +3,35 @@ import {
   StyleSheet,
   View,
   TouchableOpacity,
-  ScrollView,
+  TextInput,
   Text,
-  Image
+  Modal
 } from "react-native";
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from "react-native-responsive-screen";
-import axios from "axios";
-import { Ionicons, FontAwesome } from "@expo/vector-icons";
 
+import axios from "axios";
+
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default class Register extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+        confirmDetails: false
+    }
+  }
+
+  login() {
+    this.props.navigation.navigate("LogIn")
+  }
+
+  checkInputData(){
+
+      this.setState({confirmDetails: true})
   }
 
 
@@ -25,6 +39,43 @@ export default class Register extends React.Component {
 
     return (
       <View style={styles.container}>
+          <Text style={styles.title}>Sign Up</Text>
+
+          <View style={{display:"flex", flexDirection:"row", justifyContent:"center"}}>
+              <Text style={styles.question}>Already a user?</Text>
+              <Text style={styles.nav} onPress={() => this.login()}>Sign In</Text>
+          </View>
+
+          <Text style={styles.text}>First Name:</Text>
+          <TextInput
+          style={styles.placeholder}
+          placeholder="First Name"/>
+
+          <Text style={styles.text}>Last Name:</Text>
+          <TextInput
+          style={styles.placeholder}
+          placeholder="Last Name"/>
+
+          <Text style={styles.text}>Email address:</Text>
+          <TextInput
+          style={styles.placeholder}
+          placeholder="someone@example.com"/>
+
+          <Text style={styles.text}>Password:</Text>
+          <TextInput
+          style={styles.placeholder}
+          placeholder="**********"/>
+
+          <Text style={styles.text}>Confirm Password:</Text>
+          <TextInput
+          style={styles.placeholder}
+          placeholder="**********"/>
+
+          <TouchableOpacity style={styles.button} onPress={() => this.checkInputData()}>
+            <Text style={styles.btnText}>Next</Text>
+          </TouchableOpacity>
+
+
       </View>
     );
   }
@@ -33,8 +84,51 @@ export default class Register extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    
+    marginTop: hp("8%")    
   },
+  title: {
+    textAlign: 'center',
+    fontFamily: "Nunito_700Bold",
+    fontSize: hp("3.5%"),
+    marginBottom: hp("5%")
+  },
+  question: {
+      fontFamily: "Nunito_600SemiBold",
+      fontSize: hp("2%"),
+      marginBottom: hp("5%"),
+      marginRight: wp("5%")
+  },
+  nav: {
+      fontFamily: "Nunito_300Light_Italic",
+      fontSize: hp("2%"),
+      marginBottom: hp("5%"),
+      marginRight: wp("5%"),
+      textDecorationLine: "underline"
+  },
+  text: {
+      fontFamily: "Nunito_700Bold",
+      marginBottom: hp("1%"),
+      marginLeft: wp("3%")
+  },
+  placeholder: {
+    height: hp("5%"),
+    borderWidth: 1,
+    padding: wp("2%"),
+    marginHorizontal: wp("3%"),
+    marginBottom: hp("3%")
+  },
+  button: {
+    width: wp("50%"),
+    padding: hp("1%"),
+    backgroundColor: "#34359A",
+    borderRadius: wp("50%"),
+    marginLeft: wp("45%"),
+    marginTop: hp("3%"),
+  },
+  btnText:{
+    fontFamily:"Nunito_400Regular",
+    fontSize: hp("2.5%"),
+    color: "#ffffff",
+    textAlign: "center"
+  }
 });
