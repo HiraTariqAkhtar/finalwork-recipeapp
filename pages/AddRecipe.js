79fd574379ec2ac2bb860c16e1b8cbff1828258c
HiraTariqAkhtar <hira.akhtar@student.ehb.be>
2023-06-13 12:51:54 +0200

    Create all textfields for adding a new recipe

--- pages/AddRecipe.js
+++ pages/AddRecipe.js
@@ -2,16 +2,158 @@ import React from "react";
 import {
   StyleSheet,
   View,
+  Text,
+  ScrollView,
+  TextInput,
+  TouchableOpacity,
+  ToastAndroid
 } from "react-native";
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from "react-native-responsive-screen";
 
+import { Ionicons } from "@expo/vector-icons";
+import CheckBox from 'react-native-check-box'
+
+
+import { collection, getDocs, addDoc } from "firebase/firestore"; 
+import {DATABASE} from "../firebaseConfig"
+
 
 export default class AddRecipe extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+        servings: 0,
+        recipeName: "",
+        ingredients: [{
+            id: 0,
+            name: "",
+            quantity: ""
+        }],
+        instructions: [{
+            number: 0, 
+            step: "",
+        }],
+        culture: [],
+        time: 0,
+        foodImg: "",
+        dishTypes: [],
+        period: [],
+
+        cultureSelection: [],
+        dishTypeSelection: [],
+        occasionSelection: [],
+
+        cultureCheckedInFilter: [],
+        dishTypeCheckedInFilter: [],
+        occasionCheckedInFilter: [],
+    }
+
+    this.getAllFilters()
+  }
+
+  async getAllFilters() {
+    let cultures = []
+    let dishTypes = []
+    let occasions = []
+
+    cultureChecked = []
+    dishTypeChecked = []
+    occasionChecked = []
+
+    // Push values from database into arrays + set checkbox to unchecked
+    let cultureCollection = collection(DATABASE, "cultures")
+    let cultureData = await getDocs(cultureCollection)
+    if (cultureData.size > 0) {
+      cultureData.forEach((doc) => {
+        cultures.push(doc.data().culture)
+        cultureChecked.push(false)
+      })
+    } 
+
+    let dishTypeCollection = collection(DATABASE, "dishTypes")
+    let dishTypeData = await getDocs(dishTypeCollection)
+    if (dishTypeData.size > 0) {
+      dishTypeData.forEach((doc) => {
+        dishTypes.push(doc.data().dishType)
+        dishTypeChecked.push(false)
+      })
+    } 
+
+    let occasionCollection = collection(DATABASE, "occasions")
+    let occasionData = await getDocs(occasionCollection)
+    if (occasionData.size > 0) {
+      occasionData.forEach((doc) => {
+        occasions.push(doc.data().occasion)
+        occasionChecked.push(false)
+      })
+    } 
+
+    // order arrays alphabetically
+    cultures.sort()
+    dishTypes.sort()
+    occasions.sort()
+
+    // save data arrays in state
+    this.setState({cultureSelection: cultures})
+    this.setState({dishTypeSelection: dishTypes})
+    this.setState({occasionSelection: occasions})
+
+    this.setState({cultureCheckedInFilter: cultureChecked})
+    this.setState({dishTypeCheckedInFilter: dishTypeChecked})
+    this.setState({occasionCheckedInFilter: occasionChecked})
+  }
+
+  addFilter(checkedArray, i, item, arrayToAdd){
+    checkedArray[i] = !checkedArray[i]
+    if(!arrayToAdd.includes(item)){
+        arrayToAdd.push(item)
+    } else {
+        arrayToAdd = arrayToAdd.filter(remove => remove != item)
+    }
+    //console.log(arrayToAdd)
+    this.setState({checkedArray: checkedArray})
+    this.setState({arrayToAdd: arrayToAdd})
+  }
+
+  addIngredient(ingredient, index, param) {
+      let ingredients= [... this.state.ingredients]
+      ingredients[index][param] = ingredient
+
+      //console.log(ingredients)
+      this.setState({ingredients: ingredients})
+  }
+
+  addNewIngredient(index){
+      let ingredients= [... this.state.ingredients]
+      ingredients.push({
+          id: index+1,
+          name:"",
+          quantity:""
+      })
+
+      this.setState({ingredients: ingredients})
+  }
+
+  addInstruction(instruction, index) {
+      let instructions= [... this.state.instructions]
+      instructions[index].number = index+1
+      instructions[index].step = instruction
+
+      //console.log(instructions)
+      this.setState({instructions: instructions})
+  }
+
+  addNewInstruction(index){
+    let instructions= [... this.state.instructions]
+    instructions.push({
+          number: index+2,
+          step:""
+      })
+
+      this.setState({ instructions: instructions });
   }
 
 
@@ -19,16 +161,195 @@ export default class AddRecipe extends React.Component {
 
     return (
       <View style={styles.container}>
+          <View style={styles.header}>
+          <Ionicons
+              name={"arrow-back"}
+              size={hp("5%")}
+              color="#FF5E00"
+              onPress={() => this.props.navigation.goBack()}
+            />
+          <Text style={styles.pageTitle}>Add a new recipe</Text>
+        </View>
+
+        <ScrollView style={{marginTop: hp("3%")}} nestedScrollEnabled>
+            <Text style={styles.text}>Recipe name:</Text>
+            <TextInput
+            style={styles.placeholder}
+            placeholder="Recipe Name"
+            onChangeText={(txt) => this.setState({recipeName: txt})}/>
+            
+            <Text style={styles.text}>Servings:</Text>
+            <TextInput
+            style={styles.placeholder}
+            placeholder="0"
+            inputMode="numeric"
+            onChangeText={(txt) => this.setState({servings: parseInt(txt)})}/>
+            
+            <Text style={styles.text}>Time needed (in minutes):</Text>
+            <TextInput
+            style={styles.placeholder}
+            placeholder="0 minutes"
+            inputMode="numeric"
+            onChangeText={(txt) => this.setState({time: parseInt(txt)})}/>
+
+            <Text style={styles.text}>Select all possible filters</Text>
+            
+                <Text style={styles.category}>Culture:</Text>
+                <ScrollView style={styles.filterChoice} nestedScrollEnabled>
+                {this.state.cultureSelection.map((culture, index) => 
+                <View style={styles.iconText}>
+                  <CheckBox
+                  style={{marginLeft: wp("3%")}}
+                  isChecked = {this.state.cultureCheckedInFilter[index]}
+                  onClick= {() => this.addFilter(this.state.cultureCheckedInFilter, index, culture, this.state.culture)}/>
+                  <Text style={styles.text}>{culture}</Text>
+                </View>)}
+              </ScrollView>
+  
+                <Text style={styles.category}>Dish Type:</Text>
+                <ScrollView style={styles.filterChoice} nestedScrollEnabled>
+                  {this.state.dishTypeSelection.map((type, index) => 
+                    <View style={styles.iconText}>
+                      <CheckBox
+                      style={{marginLeft: wp("3%")}}
+                      isChecked = {this.state.dishTypeCheckedInFilter[index]}
+                      onClick= {() => this.addFilter(this.state.dishTypeCheckedInFilter, index, type, this.state.dishTypes)}/>
+                      <Text style={styles.text}>{type}</Text>
+                    </View>)}
+                </ScrollView>
+  
+                <Text style={styles.category}>Occasion:</Text>
+                <ScrollView style={styles.filterChoice} nestedScrollEnabled>
+                  {this.state.occasionSelection.map((occasion, index) => 
+                  <View style={styles.iconText}>
+                    <CheckBox
+                    style={{marginLeft: wp("3%")}}
+                    isChecked = {this.state.occasionCheckedInFilter[index]}
+                    onClick= {() => this.addFilter(this.state.occasionCheckedInFilter, index, occasion, this.state.period)}/>
+                    <Text style={styles.text}>{occasion}</Text>
+                  </View>)}
+                </ScrollView>
+
+            <Text style={styles.text}>Ingredients</Text>
+            {this.state.ingredients.map((ingredient, index) => (
+                <View style={{display:"flex", flexDirection:"row"}} key={index}>
+                <TextInput
+                style={[styles.placeholder, {width:wp("20%")}]}
+                placeholder="quantity"
+                value={ingredient.quantity}
+                onChangeText={(txt) => this.addIngredient(txt, index, "quantity")}/>
+                <TextInput
+                style={[styles.placeholder, {width:wp("55%")}]}
+                placeholder="ingredient"
+                value={ingredient.name}
+                onChangeText={(txt) => this.addIngredient(txt, index, "name")}/>
+
+                {index == this.state.ingredients.length-1 &&(
+                <Ionicons
+                name={"add"}
+                size={hp("5%")}
+                color="#FF5E00"
+                onPress={() => this.addNewIngredient(index)}/>
+                )}
+            </View>
+            ))}
+
+            <Text style={styles.text}>Instructions</Text>
+            {this.state.instructions.map((instruction, index) => (
+                <View style={[styles.iconText, {display:"flex", flexDirection:"row"}]} key={index}>
+                <Text style={styles.text}>{index + 1}</Text>
+                <TextInput
+                style={[styles.placeholder, {width:wp("75%")}]}
+                placeholder="To do"
+                value={instruction.step}
+                onChangeText={(txt) => this.addInstruction(txt, index)}/>
+
+                {index == this.state.instructions.length-1 &&(
+                <Ionicons
+                name={"add"}
+                size={hp("5%")}
+                color="#FF5E00"
+                onPress={() => this.addNewInstruction(index)}/>
+                )}
+            </View>
+            ))}
+
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.btnText}>Add recipe</Text>
+          </TouchableOpacity> 
+          </ScrollView>
+
+
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    
-  },
+    container: {
+        flex: 1,
+        paddingBottom: hp("5%"),
+        paddingTop: hp("5%"),
+        backgroundColor:"#FFFFFF"    
+      },
+    header: {
+        display:"flex",
+        flexDirection:"row",
+        marginHorizontal: wp("7.5%")
+    },
+    pageTitle: {
+        fontFamily: "Nunito_700Bold",
+        fontSize: hp("3.5%"),
+        color:"#FF0000",
+        marginLeft: wp("10%")
+    },
+    text: {
+        fontFamily: "Nunito_700Bold",
+        marginBottom: hp("1%"),
+        marginLeft: wp("3%")
+    },
+    placeholder: {
+        height: hp("5%"),
+        borderWidth: 1,
+        padding: wp("2%"),
+        marginHorizontal: wp("3%"),
+        marginBottom: hp("2%"),
+        borderColor: "#FF5E00",
+        borderRadius: 10,
+    },
+    category: {
+        fontFamily: "Nunito_400Regular",
+        marginLeft: wp("6%")
+
+    },
+    filterChoice: {
+        marginBottom:hp("2%"),
+        height: hp("15%"),
+        borderRadius: 10,
+        borderColor: "#FF5E00",
+        borderWidth: 1,
+        width: wp("80%"),
+        marginHorizontal: wp("10%"),
+    },
+    iconText: {
+        display: "flex",
+        flexDirection: "row",
+        flexWrap: "wrap",
+        alignItems: "center",
+        marginBottom: hp("1%"),
+    },
+    button: {
+        width: wp("80%"),
+        padding: hp("1%"),
+        backgroundColor: "#FF5E00",
+        borderRadius: 10,
+        marginTop: hp("5%"),
+        marginHorizontal: wp("10%")
+      },
+    btnText:{
+        fontFamily:"Nunito_400Regular",
+        fontSize: hp("2.5%"),
+        color: "#ffffff",
+        textAlign: "center"
+    },
 });
