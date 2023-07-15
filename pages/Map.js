cbce3de83a2656f81335a3824d4b6723b60eb882
HiraTariqAkhtar <hira.akhtar@student.ehb.be>
2023-07-15 18:25:30 +0200

    Add select dropdown

--- pages/Map.js
+++ pages/Map.js
@@ -13,11 +13,20 @@ import {
 } from "react-native-responsive-screen";
 import axios from "axios";
 import { Ionicons, FontAwesome } from "@expo/vector-icons";
+import SelectDropdown from 'react-native-select-dropdown'
 
 
 export default class Map extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      mapOptions: ["All", "Restaurants", "Supermarkets"]
+    }
+  }
+
+  async filterResults(selected) {
+    console.log(selected)
   }
 
 
@@ -26,6 +35,20 @@ export default class Map extends React.Component {
     return (
       <View style={styles.container}>
           <Text style={styles.title}>Map</Text>
+          <SelectDropdown
+          buttonStyle = {styles.dropDown}
+          buttonTextStyle = {styles.dropDownText}
+          dropdownStyle = {styles.dropDownOptions}
+          rowTextStyle = {styles.dropDownText}
+          data = {this.state.mapOptions}
+          onSelect={(selectedItem) => {
+            this.filterResults(selectedItem)
+          }}
+          defaultValue = "All"
+          renderDropdownIcon={isOpened => {
+            return <FontAwesome name={isOpened ? 'chevron-up' : 'chevron-down'} color={'#115740'} size={20} />;
+          }}/>
+
       </View>
     );
   }
@@ -44,4 +67,21 @@ const styles = StyleSheet.create({
         fontSize: hp("3.5%"),
         color: "#FF0000"
       },
+      dropDown: {
+        width: wp("80%"),
+        backgroundColor: "#fff",
+        borderRadius: 10,
+        borderColor: "#115740",
+        borderWidth: 2,
+        marginHorizontal: wp("10%"),
+        marginTop: hp("3%")
+      },
+      dropDownOptions: {
+        backgroundColor: "#fff",
+        borderRadius: 10,
+      },
+      dropDownText: {
+        fontFamily: "Nunito_700Bold",
+        fontSize: hp("2.5%"),
+      }
 });
