0d81f2279e4c83999f2a4e9870d8fabb58331149
HiraTariqAkhtar <hira.akhtar@student.ehb.be>
2023-07-16 12:00:11 +0200

    Add legend to map page

--- pages/Map.js
+++ pages/Map.js
@@ -35,20 +35,49 @@ export default class Map extends React.Component {
     return (
       <View style={styles.container}>
           <Text style={styles.title}>Map</Text>
-          <SelectDropdown
-          buttonStyle = {styles.dropDown}
-          buttonTextStyle = {styles.dropDownText}
-          dropdownStyle = {styles.dropDownOptions}
-          rowTextStyle = {styles.dropDownText}
-          data = {this.state.mapOptions}
-          onSelect={(selectedItem) => {
-            this.filterResults(selectedItem)
-          }}
-          defaultValue = "All"
-          renderDropdownIcon={isOpened => {
-            return <FontAwesome name={isOpened ? 'chevron-up' : 'chevron-down'} color={'#115740'} size={20} />;
-          }}/>
+          <View style={{display: "flex", flexDirection: "row"}}>
+            <SelectDropdown
+            buttonStyle = {styles.dropDown}
+            buttonTextStyle = {styles.dropDownText}
+            dropdownStyle = {styles.dropDownOptions}
+            rowTextStyle = {styles.dropDownText}
+            data = {this.state.mapOptions}
+            onSelect={(selectedItem) => {
+              this.filterResults(selectedItem)
+            }}
+            defaultValue = "All"
+            renderDropdownIcon={isOpened => {
+              return <FontAwesome name={isOpened ? 'chevron-up' : 'chevron-down'} color={'#115740'} size={20} />;
+            }}/>
 
+            <View style={styles.legend}>
+              <View style={styles.iconText}>
+                <Ionicons
+                  name={"location"}
+                  size={hp("4%")}
+                  color="#FF0000"
+                />
+                <Text style={styles.legendText}>Your location</Text>
+              </View>
+              <View style={styles.iconText}>
+                <Ionicons
+                  name={"cart"}
+                  size={hp("4%")}
+                  color="#115740"
+                />
+                <Text style={styles.legendText}>Supermarkets</Text>
+              </View>
+              <View style={styles.iconText}>
+                <FontAwesome
+                  name={"cutlery"}
+                  size={hp("3.5%")}
+                  color="#FF5E00"
+                  marginHorizontal={wp("1%")}
+                />
+                <Text style={styles.legendText}>Restaurants</Text>
+              </View>
+            </View>
+          </View>
       </View>
     );
   }
@@ -68,12 +97,12 @@ const styles = StyleSheet.create({
         color: "#FF0000"
       },
       dropDown: {
-        width: wp("80%"),
+        width: wp("40%"),
         backgroundColor: "#fff",
         borderRadius: 10,
         borderColor: "#115740",
         borderWidth: 2,
-        marginHorizontal: wp("10%"),
+        marginHorizontal: wp("5%"),
         marginTop: hp("3%")
       },
       dropDownOptions: {
@@ -83,5 +112,24 @@ const styles = StyleSheet.create({
       dropDownText: {
         fontFamily: "Nunito_700Bold",
         fontSize: hp("2.5%"),
-      }
+      },
+      iconText: {
+        display: "flex",
+        flexDirection: "row",
+        flexWrap: "wrap",
+        alignItems: "center",
+        marginBottom: hp("1%"),
+      },
+      legend: {
+        width: wp("40%"),
+        marginTop: hp("3%"),
+        borderRadius: 10,
+        borderWidth: 2,
+        borderColor: "#115740",
+        padding: 5
+      },
+      legendText: {
+        fontFamily: "Nunito_400Regular",
+        marginLeft: wp("3%")
+      },
 });
