796886040e75281c5b4f82f6ecf37feea9706aa2
HiraTariqAkhtar <hira.akhtar@student.ehb.be>
2023-07-17 12:49:06 +0200

    Use firebase/auth for creating users

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -2,7 +2,7 @@ import { initializeApp } from 'firebase/app';
 import {FIREBASE_API_KEY , FIREBASE_AUTH_DOMAIN, FIREBASE_PROJECT_ID, FIREBASE_STORAGE_BUCKET, FIREBASE_MESSAGING_SENDER_ID, FIREBASE_APP_ID, FIREBASE_MEASUREMENT_ID} from '@env'
 
 // Optionally import the services that you want to use
-// import {...} from "firebase/auth";
+import {getAuth} from "firebase/auth";
 // import {...} from "firebase/database";
 import {getFirestore} from "firebase/firestore";
 // import {...} from "firebase/functions";
@@ -23,3 +23,4 @@ const app = initializeApp(firebaseConfig);
 // For more information on how to access Firebase in your project,
 // see the Firebase documentation: https://firebase.google.com/docs/web/setup#access-firebase
 export const DATABASE = getFirestore(app);
+export const AUTH = getAuth(app);
