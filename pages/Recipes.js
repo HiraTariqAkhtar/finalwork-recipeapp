7229ed68f3caab09d15d8e0f11c05acb6553c94f
HiraTariqAkhtar <hira.akhtar@student.ehb.be>
2023-07-18 18:58:22 +0200

    Recipes: Remove category from state

--- pages/Recipes.js
+++ pages/Recipes.js
@@ -43,7 +43,6 @@ export default class Recipes extends React.Component {
           img: "",
         },
       ],
-      categorieSelectedFromHome: ""
 
       // allCultures:[],
       // allDishTypes:[],
