ff970ee17ab136f6cf55cf5d93c142f15301fae5
HiraTariqAkhtar <hira.akhtar@student.ehb.be>
2023-07-22 17:40:13 +0200

    Login/Register: Give textInput fields same border width as boxes

--- pages/Register.js
+++ pages/Register.js
@@ -349,7 +349,7 @@ const styles = StyleSheet.create({
   },
   placeholder: {
     height: hp("5%"),
-    borderWidth: 1,
+    borderWidth: 3,
     padding: wp("2%"),
     marginHorizontal: wp("3%"),
     marginBottom: hp("3%"),
